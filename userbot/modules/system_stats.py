9e3b5169c4a058eca1780730f44ca3fcc1a1f187
ronaldyganteng <77110439+ronaldyganteng@users.noreply.github.com>
2021-02-15 03:03:59 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -135,11 +135,11 @@ async def amireallyalive(alive):
     output = (
         f"`WeebProject` is running on`\n"
         f"==========================\n"
-        f"┣[`𝓢𝓹𝓮𝓬𝓲𝓪𝓵 𝓔𝓭𝓲𝓽𝓲𝓸𝓷`\n"
-        f"┣[ 🧸 User        : {DEFAULTUSER}\n"
-        f"┣[ 🐍 Python      : {python_version()}\n"
-        f"┣[ ⚙️ Telethon    : {version.__version__}\n"
-        f"┣[ 💻 Running on  : {repo.active_branch.name}\n"
+        f"`𝓢𝓹𝓮𝓬𝓲𝓪𝓵 𝓔𝓭𝓲𝓽𝓲𝓸𝓷`\n"
+        f"- 🧸 User        : {DEFAULTUSER}\n"
+        f"- 🐍 Python      : {python_version()}\n"
+        f"- ⚙️ Telethon    : {version.__version__}\n"
+        f"- 💻 Running on  : {repo.active_branch.name}\n"
         f"==========================\n"
     )
     if ALIVE_LOGO:
