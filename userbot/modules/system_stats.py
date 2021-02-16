7612f28dc50e55e024a09aaf9dd885f4efbac623
ronaldyganteng <77110439+ronaldyganteng@users.noreply.github.com>
2021-02-16 00:45:59 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -24,34 +24,6 @@ repo = Repo()
 # ============================================
 
 
-async def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    up_time += ":".join(time_list)
-
-    return up_time
-
-
 @register(outgoing=True, pattern=r"^\.sysd$")
 async def sysdetails(sysd):
     """ For .sysd command, get system info using neofetch. """
@@ -167,7 +139,6 @@ async def amireallyalive(alive):
         f"🐍 `Python                   :` v{python_version()}\n"
         f"⚙️ `Telethon                :` v{version.__version__}\n"
         f"💉 `Archiestyc             :` {repo.active_branch.name}`\n"
-        f"🕒 Bot Uptime               : {uptime}\n"
         "`====================================`\n"
     )
     if ALIVE_LOGO:
