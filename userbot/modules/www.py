c688d5ac14889aaeab48f4df85a4afaed638998b
ronaldyganteng <77110439+ronaldyganteng@users.noreply.github.com>
2021-02-16 10:22:51 +0000

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -71,10 +71,10 @@ async def neardc(event):
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
-    await pong.edit("`Pong!`")
+    await pong.edit("`Pong, Kecepatan Internet Kamu!`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit("`Pong!\n%sms`" % (duration))
+    await pong.edit("`Pong, Kecepatan Internet Kamu!\n%sms`" % (duration))
 
 
 CMD_HELP.update(
