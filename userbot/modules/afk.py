a9dcdcca4a16a87ad2c4ce3a1abb36cd0a7b4de1
ronaldyganteng <enambacot@gmail.com>
2021-02-16 06:40:56 +0000

    pylint: auto fixes
    Signed-off-by: ronaldyganteng <enambacot@gmail.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -277,9 +277,9 @@ async def type_afk_is_not_true(notafk):
 CMD_HELP.update(
     {
         "afk": ">` .off [Alasan Opsional] `"
-         "\ nPenggunaan: Menetapkan Anda sebagai afk. \ nBalas kepada siapa saja yang menandai / PM"
-         "Anda memberi tahu mereka bahwa Anda AFK (alasan)."
-         "\ n \ n>` .unoff` "
-         "\ nPenggunaan: Mematikan Anda dari mode AFK"
+        "\\ nPenggunaan: Menetapkan Anda sebagai afk. \\ nBalas kepada siapa saja yang menandai / PM"
+        "Anda memberi tahu mereka bahwa Anda AFK (alasan)."
+        "\\ n \\ n>` .unoff` "
+        "\\ nPenggunaan: Mematikan Anda dari mode AFK"
     }
 )
